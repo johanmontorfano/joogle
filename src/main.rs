4bb567c4d5081a36adbf163d28d7ed1739763c53
= <me@johanmontorfano.com>
2024-06-25 22:51:01 +0200

    change: DB_POOL connection type

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 #![feature(thread_sleep_until)]
 extern crate r2d2;
-extern crate r2d2_sqlite;
-extern crate rusqlite;
 #[macro_use] extern crate rocket;
 
 mod sanitize;
@@ -16,8 +14,8 @@ mod error;
 
 use lazy_static::lazy_static;
 use maud::Markup;
-use r2d2_sqlite::SqliteConnectionManager;
-use r2d2::Pool;
+use r2d2::{ManageConnection, Pool};
+use r2d2_postgres::{postgres::NoTls, PostgresConnectionManager};
 use rocket::{fs::{FileServer, relative}, serde::json::Json};
 use searching::feeling_lucky;
 use templates::{indexing::indexing_page, search::search_result_page};
@@ -25,13 +23,11 @@ use indexer::url::QueueBot;
 use indexer::sitemaps::SitemapBot;
 
 lazy_static! {
-    static ref DB_POOL: r2d2::Pool<r2d2_sqlite::SqliteConnectionManager> = {
-        let manager = SqliteConnectionManager::file("index_db.db")
-            .with_init(|c| c.execute_batch("
-                PRAGMA synchronous = off;
-                PRAGMA encoding = 'UTF-16';
-                PRAGMA journal_mode = WAL;
-            "));
+    static ref DB_POOL: Pool<PostgresConnectionManager<NoTls>> = {
+        let manager = PostgresConnectionManager::new(
+            "host=localhost user=postgres".parse().unwrap(),
+            NoTls
+        );
         Pool::new(manager).unwrap()
     };
     static ref SITEMAP_BOT: indexer::sitemaps::SitemapBot = {
