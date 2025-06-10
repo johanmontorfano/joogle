727bead4d1c17076c47e7d07fc41d9f3aa74f6d8
Johan Montorfano <me@johanmontorfano.com>
2025-06-10 03:31:24 +0200

    rm: file cache since it was messing up the toolchain used

--- src/pages/console/mod.rs
+++ src/pages/console/mod.rs
@@ -1,26 +1,8 @@
-mod iex;
-mod sublink;
-use iex::mod_indexing_expl;
-use maud::{Markup, DOCTYPE};
-use sublink::mod_submit_link;
+use std::path::Path;
 
 /// To ensure ease of development, every part of the console will be a module,
 /// making the console ui 100% modulable.
 #[get("/search/console")]
-pub fn console_ui() -> Markup {
-    html! {
-        html {
-            (DOCTYPE)
-            head {
-                title { "Joogle Search Console" }
-                link rel="stylesheet" href="/static/root.css";
-                link rel="stylesheet" href="/static/dashboard.css";
-            }
-            body {
-                h1 { "JOOGLE SEARCH CONSOLE" }
-                div { (mod_submit_link()) }
-                div { (mod_indexing_expl()) }
-            }
-        }
-    }
+pub fn console_ui() -> String {
+    "SEARCH CONSOLE APP SHOULD BE INCLUDED".to_string()
 }
