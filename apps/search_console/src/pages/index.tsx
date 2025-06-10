37d830ec36475446714d6f03aa3ad4d205d0986e
Johan Montorfano <me@johanmontorfano.com>
2025-06-10 16:07:50 +0200

    fix: better layout

--- apps/search_console/src/pages/index.tsx
+++ apps/search_console/src/pages/index.tsx
@@ -1,13 +1,5 @@
 import { useIndexSysStats } from "../hooks/use_stats"
 
 export function RootIndex() {
-    const [queueLength, indexedUrls, tendency] = useIndexSysStats();
-
-    return <div>
-        <h1>Dashboard</h1>
-        <div>
-            <p>Queue length: {queueLength()} ({tendency()}/s)</p>
-            <p>Indexed URLs: {indexedUrls()}</p>
-        </div>
-    </div>
+    return <div />
 }
--- apps/search_console/src/pages/layout.tsx
+++ apps/search_console/src/pages/layout.tsx
@@ -1,7 +1,7 @@
 import { A, RouteSectionProps } from "@solidjs/router";
 
 export function RootLayout(props: RouteSectionProps<any>) {
-    return <div class="w-full min-h-dvh">
+    return <div class="w-full min-h-dvh flex flex-col">
         <div class="w-full border-b border-gray-500 p-2 flex justify-between">
             <p class="logo_like">
                 <a onClick={() => window.location.assign("/")} href="/" class="logo_like hover:text-red-300">JOOGLE</a>
@@ -13,8 +13,13 @@ export function RootLayout(props: RouteSectionProps<any>) {
             </nav>
             <p class="logo_like text-transparent select-none">JOOGLE Search Console</p>
         </div>
-        <div class="p-2">
+        <div class="flex flex-1 justify-self-stretch">
+            <div class="p-2 min-w-[250px] max-w-[350px] w-[20vw] max-lg:hidden border-r border-gray-500">
+                OWNED DOMAINS
+            </div>
+            <div class="p-2 w-full">
             {props.children}
+            </div>
         </div>
     </div>
 }
