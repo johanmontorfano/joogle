a08da56ae7b176819a45d45a4170fb20ba212eb6
Johan Montorfano <me@johanmontorfano.com>
2025-06-12 17:34:11 +0200

    fix: now using new schemas mod struct

--- src/models.rs
+++ src/models.rs
@@ -1,7 +1,7 @@
 use rocket_db_pools::diesel::prelude::*;
 
 #[derive(Insertable)]
-#[diesel(table_name = crate::schema::domains)]
+#[diesel(table_name = crate::schema::_public::domains)]
 pub struct AddDomainOwnership {
     pub domain: String,
     pub owned_by: uuid::Uuid,
