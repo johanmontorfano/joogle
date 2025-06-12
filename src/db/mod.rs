2fee6ecbd46c21684ce8a5fbe3f650652e659164
Johan Montorfano <me@johanmontorfano.com>
2025-06-13 01:26:00 +0200

    add: jwt_auth mod

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -1,9 +1,12 @@
 // This module is only about local file storage.
 pub mod local;
 
+// Each of those modules are about a functionality linked to data stored on the
+// database, and not related to specific routes.
+pub mod jwt_auth;
+
 // Each table of the database has it's own module here. Thus, the module `sites`
 // contains actions that can be done on the `sites` table.
-
 pub mod sites;
 pub mod domains;
 #[path = "./_[word].rs"]
