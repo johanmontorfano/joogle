8029a9ded5786f5d3d010c8a078e9858214d0b70
Johan Montorfano <me@johanmontorfano.com>
2025-06-12 06:06:53 +0200

    add: unique constraint for the domain key

--- migrations/2025-06-12-005255_domains/up.sql
+++ migrations/2025-06-12-005255_domains/up.sql
@@ -3,6 +3,6 @@ create table public.domains (
   id uuid not null default gen_random_uuid(),
   created_at timestamp with time zone not null default now(),
   owned_by uuid not null,
-  domain text not null,
+  domain text unique not null,
   constraint domains_pkey primary key (id)
 ) TABLESPACE pg_default;
