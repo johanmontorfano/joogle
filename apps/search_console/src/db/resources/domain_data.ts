4d748c77068321c1ab8d8dd03c0a6ae4135d4425
Johan Montorfano <me@johanmontorfano.com>
2025-06-12 06:05:40 +0200

    fix: issue with userData when the auth flow is automatic

--- apps/search_console/src/db/resources/domain_data.ts
+++ apps/search_console/src/db/resources/domain_data.ts
@@ -8,15 +8,21 @@ export interface DomainTableEntry {
     domain: string;
 }
 
+/// Will load all domains owned by the user from supabase.
 export async function fetchDomains(): Promise<DomainTableEntry[]> {
+    const { data: { user } } = await supabase.auth.getUser();
+
+    if (user === null)
+        return [];
+
     const {data, error} = await supabase.from("domains")
         .select("*")
-        .eq("owned_by", userData().id);
+        .eq("owned_by", user.id);
 
     if (error)
         throw new Error(error.message);
 
-    return data;
+    return data || [];
 }
 
 export const [domains, { refetch: getDomains }] = createResource(fetchDomains);
