0e7710c97df9890b100c4ca122efd957883d1a4a
Johan Montorfano <me@johanmontorfano.com>
2025-06-12 06:47:35 +0200

    fix: design and order of buttons

--- apps/search_console/src/components/domains_list.tsx
+++ apps/search_console/src/components/domains_list.tsx
@@ -1,12 +1,24 @@
 import { A } from "@solidjs/router";
+import { IoAddOutline } from "solid-icons/io";
 import { For } from "solid-js";
 import { domains } from "../db/resources/domain_data";
 
 export function DomainsList() {
     return <div>
-        <For each={domains()}>
-            {d => <p>{d.domain}</p>}
+        <A href="/search/console/new"
+            class="w-full p-2 flex justify-between items-center rounded-lg border dark:border-neutral-700 hover:bg-neutral-100 hover:dark:bg-neutral-800 input"
+        >
+            New Domain
+            <IoAddOutline size={22} />
+        </A>
+        <br />
+        <p class="font-semibold">
+            Owned domains
+        </p>
+        <For each={domains()} fallback={<p>No domain owned :(</p>}>
+            {d => <A href={`/search/console/${d.domain}`}>
+                {d.domain}
+            </A>}
         </For> 
-        <A href="/search/console/new">New Domain</A>
     </div>
 }
