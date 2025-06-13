5e5a234c0cce9d71a896061141f4d01ef0c8e3f7
Johan Montorfano <me@johanmontorfano.com>
2025-06-13 07:42:34 +0200

    fix: all routes of /search/console now points towards the index file of the search console SPA to prevent issues with the SPA router routes only virtually existing

--- src/pages/console.rs
+++ src/pages/console.rs
@@ -4,7 +4,7 @@ const SC_INDEX: &str = include_str!("../../static/sc-index.html");
 
 /// To ensure ease of development, every part of the console will be a module,
 /// making the console ui 100% modulable.
-#[get("/search/console")]
+#[get("/search/console/<_..>")]
 pub fn console_ui() -> content::RawHtml<&'static str> {
     content::RawHtml(SC_INDEX)
 }
